6726d19c66c2b882a5079ab4e19dce4ea98bde70
Krazune <krazune@gmail.com>
2021-02-16 14:07:33 +0000

    Add system import

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Artistas
 {
-	[System.Serializable]
+	[Serializable]
 	struct InspectorItemListEntry
 	{
 		public DummyItemSO item;
