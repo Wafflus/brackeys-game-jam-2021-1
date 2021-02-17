8346fe7a3b4048138892ae7521c4a73dbcf1eed8
Wafflus <wafflusdev@gmail.com>
2021-02-17 14:06:49 +0000

    Update explosive scriptable object script

--- Assets/Scripts/ScriptableObjects/ExplosiveSO.cs
+++ Assets/Scripts/ScriptableObjects/ExplosiveSO.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Artistas
@@ -5,21 +6,33 @@ namespace Artistas
     [CreateAssetMenu(fileName = "New Explosive", menuName = "Artistas/Explosive/New Explosive")]
     public class ExplosiveSO : ScriptableObject
     {
+        [Header("General Data")]
         public Sprite icon;
         public string title;
         [TextArea] public string description;
 
+        [Header("Collision Layers")]
         public LayerMask explosiveCollisionLayer;
         public LayerMask propsCollisionLayer;
 
+        [Header("Explosion Data")]
         [Min(0)] public float explosionRadius = 5f;
         [Range(0, 1)] public float instantExplosionRadius = .5f;
-
         [Min(0)] public float explosionForce = 10f;
-
         [Min(0)] public float timedExplosionTimer = 1f;
 
+        [Header("Explosion Visual Effects")]
+        public List<GameObject> explosionEffects;
+
+        [Header("Gizmos Data")]
         public GizmosData explosionGizmosData;
         public GizmosData instantExplosionGizmosData;
+
+        public GameObject GetRandomExplosionEffect()
+        {
+            int randomIndex = Random.Range(0, explosionEffects.Count);
+
+            return explosionEffects[randomIndex];
+        }
     }
 }
