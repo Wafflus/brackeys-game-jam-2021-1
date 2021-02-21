8315b37e252fa233d3a10aa87d59a4354ae57937
Wafflus <wafflusdev@gmail.com>
2021-02-21 01:49:17 +0000

    Remove impact sound unnecessary rigidbody variable

--- Assets/Scripts/Destructibles/ImpactSound.cs
+++ Assets/Scripts/Destructibles/ImpactSound.cs
@@ -1,17 +1,11 @@
 using UnityEngine;
+
 namespace Artistas
 {
 	public class ImpactSound : MonoBehaviour
 	{
 		[SerializeField] private SoundListSO soundList;
 		[SerializeField] private float minimumVelocityMagnitude = 2f;
-
-		private Rigidbody rigidBody;
-
-		public void Awake()
-		{
-			rigidBody = gameObject.GetComponent<Rigidbody>();
-		}
 
 		void OnCollisionEnter(Collision collision)
 		{
